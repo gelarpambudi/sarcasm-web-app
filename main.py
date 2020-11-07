84575dc3c211234559718478aa2fb5768ad23d6b
Gelar Pambudi <gelargelur@gmail.com>
2020-11-07 21:39:50 +0700

    add error handling

--- main.py
+++ main.py
@@ -15,14 +15,17 @@ def introLevel():
         input_text = request.form.get("input-text")
         model = request.form.get("model")
 
-        if (model == 'CNN'):
-            pred_result = predict(input_text, model)
-        elif (model == 'LSTM'):
-            pred_result = predict(input_text, model)
-        elif (model == 'GRU'):
-            pred_result = predict(input_text, model)
-
-        return render_template('home.html', text=pred_result[0], result=pred_result[1])
+        try:
+            if (model == 'CNN'):
+                pred_result = predict(input_text, model)
+            elif (model == 'LSTM'):
+                pred_result = predict(input_text, model)
+            elif (model == 'GRU'):
+                pred_result = predict(input_text, model)    
+            return render_template('home.html', text=pred_result[0], result=pred_result[1])
+        except ValueError:
+             flash(u'Input text tidak boleh kosong')
+             return render_template('home.html')
 
     else:
         return render_template('home.html')
