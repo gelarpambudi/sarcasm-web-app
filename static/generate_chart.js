65a82565a640288da42934c02f47903e26a6e907
Gelar Pambudi <gelargelur@gmail.com>
2020-11-17 10:01:09 +0700

    update generate_chart.js

--- static/generate_chart.js
+++ static/generate_chart.js
@@ -1,20 +1,22 @@
 console.log(payload)
-new Chart(document.getElementById("doughnut-chart"), {
-    type: 'doughnut',
-    data: {
-        labels: ["Sarkasme", "Bukan Sarkasme"],
-        datasets: [
-            {
-                backgroundColor: ["#106CF3", "#C7D9F3"],
-                data: payload
+
+var doughnutChart = new Chart(document.getElementById("doughnut-chart"), {
+        type: 'doughnut',
+        data: {
+            labels: ["Sarkasme", "Bukan Sarkasme"],
+            datasets: [
+                {
+                    backgroundColor: ["#106CF3", "#C7D9F3"],
+                    data: payload
+                }
+            ]
+        },
+        options: {
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Tingkat Sarkasme Kalimat'
             }
-        ]
-    },
-    options: {
-        legend: { display: false },
-        title: {
-            display: true,
-            text: 'Tingkat Sarkasme Kalimat'
         }
-    }
 });
+
