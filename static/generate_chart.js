bd2c792f6a70c2ed89f480f8e7f6f1631298901b
Gelar Pambudi <gelargelur@gmail.com>
2020-11-17 09:36:09 +0700

    update generate_chart.js

--- static/generate_chart.js
+++ static/generate_chart.js
@@ -1,33 +1,20 @@
-$(document).ready(function(){
-    var _data;
-    var _labels;
-    $.ajax({
-        url: "/",
-        type: "get",
-        data: {vals: ''},
-        success: function(response) {
-        full_data = JSON.parse(payload);
-        _data = full_data['data'];
-        },
-    });
-
-    new Chart(document.getElementById("doughnut-chart"), {
-        type: 'doughnut',
-        data: {
-            labels: ["Sarkasme", "Bukan Sarkasme"],
-            datasets: [
+console.log(payload)
+new Chart(document.getElementById("doughnut-chart"), {
+    type: 'doughnut',
+    data: {
+        labels: ["Sarkasme", "Bukan Sarkasme"],
+        datasets: [
             {
                 backgroundColor: ["#106CF3", "#C7D9F3"],
-                data: _data
+                data: payload
             }
-            ]
-        },
-        options: {
-            legend: { display: true },
-            title: {
-                display: true,
-                text: 'Tingkat Sarkasme Kalimat'
-            }
+        ]
+    },
+    options: {
+        legend: { display: false },
+        title: {
+            display: true,
+            text: 'Tingkat Sarkasme Kalimat'
         }
-    });
+    }
 });
